1fba3f09f2f4999617264cf411ae5bf95ca595b1
Marcello Fitton <macfittondev@gmail.com>
2024-10-01 17:48:26 -0700

    more shit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{ BufReader, BufWriter},
+    io::{BufReader, BufWriter},
+    process::Command,
 };
 
 use chrono::{DateTime, NaiveDate, Utc};
+use dialoguer::Select;
 use serde::{Deserialize, Serialize};
 
 fn main() {
     let mut app = App::new();
 
-    app.end_active_session();
+    app.init();
 }
 
 #[derive(Serialize, Deserialize)]
 struct App {
     data: HashMap<NaiveDate, WorkDay>,
+    today: NaiveDate,
 }
 
 impl App {
+    fn clear_console(&self) {
+        if cfg!(target_os = "windows") {
+            Command::new("cmd")
+                .args(&["/C", "cls"])
+                .status()
+                .expect("failed to clear console");
+        } else {
+            Command::new("clear")
+                .status()
+                .expect("failed to clear console");
+        }
+    }
+
     pub fn new() -> Self {
         let today = Utc::now().date_naive();
         let mut app;
@@ -28,6 +44,7 @@ impl App {
             app = App::read_data();
         } else {
             app = App {
+                today: today.clone(),
                 data: HashMap::new(),
             };
         }
@@ -47,41 +64,105 @@ impl App {
 
         app
     }
-    pub fn init(&self) {
+    // Main CLI Loop
+    pub fn init(&mut self) {
+        self.clear_console();
         loop {
-            print!("Enter command (start, end, status, quit): ");
-            io::stdout().flush().unwrap();
+            // check if an active session exists
+            let has_active_session = self.data.get(&self.today).unwrap().active_session.is_some();
+            if has_active_session {
+                println!("You have an active session");
+            }
 
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-            match input.trim() {
-                "start" => self.create_session(),
-                "end" => self.end_active_session(),
-                "quit" => break,
-                _ => println!("Unknown command"),
+            // Filter selection options
+            let opts = vec![
+                "Clock In",
+                "Clock Out",
+                "View Current Session",
+                "Get Total Working Time",
+                "Exit",
+            ]
+            .into_iter()
+            .filter(|&selection| match selection {
+                "Clock In" if has_active_session => false,
+                "Clock Out" if !has_active_session => false,
+                "View Current Session" if !has_active_session => false, // Only show if an active session exists
+                _ => true,
+            })
+            .collect::<Vec<&str>>();
+
+            // Display select menu
+            let selection = Select::new()
+                .with_prompt("What do you choose?")
+                .items(&opts)
+                .interact()
+                .unwrap();
+
+            let answer = opts[selection];
+            /* Selection Resolvers */
+
+            // Clock in
+            if answer == "Clock In" {
+                if has_active_session {
+                    println!(
+                        "You cannot clock in while there is an active session for the current day"
+                    );
+                    continue;
+                }
+
+                let sesh = self.create_session().unwrap();
+
+                println!("Session started at {}", sesh.start);
+                self.write_data();
+            }
+
+            // Clock out
+            if answer == "Clock Out" {
+                if !has_active_session {
+                    println!("You cannot clock out when there is no active session.");
+                    continue;
+                }
+
+                // End session and get clone of session
+                let sesh = self.end_active_session().unwrap();
+
+                // Display duration of time to user Hours: {} Minutes: {}
+                println!("Hours: {}, Minutes: {}", sesh.start, sesh.end.unwrap());
+            }
+
+            // Get total working time for today
+            if answer == "View Active Session" {}
+
+            // View Current Session
+            if answer == "View Current Session" {}
+            if answer == "Exit" {
+                break;
             }
         }
     }
 
-    pub fn create_session(&mut self) {
+    pub fn create_session(&mut self) -> Option<Session> {
         let work_day = self.data.get_mut(&Utc::now().date_naive()).unwrap();
 
         if work_day.active_session.is_some() {
             println!("A session is already active");
+            return None;
         } else {
             let sesh = Session {
                 start: Utc::now(),
                 end: None,
             };
 
-            work_day.sessions.push(sesh);
+            work_day.sessions.push(sesh.clone());
             work_day.active_session = Some(work_day.sessions.len() - 1);
 
             self.write_data();
+
+            return Some(sesh);
         }
     }
 
-    pub fn end_active_session(&mut self) {
+    pub fn end_active_session(&mut self) -> Option<Session> {
         let work_day = self.data.get_mut(&Utc::now().date_naive()).unwrap();
 
         if let Some(idx) = work_day.active_session {
@@ -89,7 +170,11 @@ impl App {
             work_day.active_session = None;
 
             self.write_data();
+
+            return Some(self.data.get(&self.today).unwrap().sessions[idx].clone());
         }
+
+        return None;
     }
 
     fn read_data() -> App {
@@ -106,19 +191,19 @@ impl App {
         if let Ok(file) = File::create("data.json") {
             let writer = BufWriter::new(file);
 
-            serde_json::to_writer(writer, &self);
+            let _ = serde_json::to_writer(writer, &self);
         }
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct WorkDay {
     sessions: Vec<Session>,
     active_session: Option<usize>,
     date: NaiveDate,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Session {
     start: DateTime<Utc>,
     end: Option<DateTime<Utc>>,
