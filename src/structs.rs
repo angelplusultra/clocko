5662528811a9422a28c24c5b7acd96915969c65a
Marcello Fitton <macfittondev@gmail.com>
2024-10-02 18:54:57 -0700

    Changed UTC to Local and applied Debug to structs

--- src/structs.rs
+++ src/structs.rs
@@ -1,16 +1,16 @@
-use chrono::{DateTime, NaiveDate, Utc};
+use chrono::{DateTime, Local, NaiveDate};
 use serde::{Deserialize, Serialize};
 
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct WorkDay {
     pub sessions: Vec<Session>,
     pub active_session: Option<usize>,
     pub date: NaiveDate,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Session {
-    pub start: DateTime<Utc>,
-    pub end: Option<DateTime<Utc>>,
+    pub start: DateTime<Local>,
+    pub end: Option<DateTime<Local>>,
 }
